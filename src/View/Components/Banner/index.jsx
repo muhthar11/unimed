7f496708c4c29a4cd53dc910401c12a79cde98b5
muhthar <muhammedmuhthar11@gmail.com>
2024-04-13 14:40:04 +0530

    fix: code quality

--- src/View/Components/Banner/index.jsx
+++ src/View/Components/Banner/index.jsx
@@ -1,13 +1,13 @@
-import React from "react";// Replace with your image path
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./banner.css";
 
 function Banner() {
   return (
     <div className="banner-container container-fluid px-0">
-      <div className="banner-text">
-        <h2 className="heading">MedShop</h2>
-        <p className="paragraph">
+      <div className="banner__text">
+        <h2 className="banner__heading">MedShop</h2>
+        <p className="banner__paragraph">
           One-Stop Destination for Medical Essentials and Wellness Solutions
         </p>
       </div>
--- src/View/Components/Health/index.jsx
+++ src/View/Components/Health/index.jsx
@@ -10,71 +10,51 @@ import generalHealthcareProducts from "../../Assets/Images/generalHealthcareProd
 function Health() {
   return (
     <div className="container-fluid px-0">
-      <div className="health-container ">
-        <div className="health-container-section1 ">
-          <img className="health" src={health} alt="health" />
+      <div className="row health-container">
+        <div className="col-md-6 health-container__section1">
+          <img className="health-container__image" src={health} alt="health" />
         </div>
-        <div className="health-container-section2">
-          <div className="heading">
-            <p className="heading-text">
+        <div className="col-md-6 health-container__section2">
+          <div className="health-container__heading">
+            <p className="health-container__heading-text">
               Find All The<span>Health Essentials Here</span>
             </p>
           </div>
-          <div className="dataSection">
-          <div className="healthEssentials col-4">
-            <div className="card cardBorderColor2">
-              <img
-                className="icon"
-                src={medicalEquipment}
-                alt="medicalEquipment"
-              />
+          <div className="row health-container__dataSection">
+            { /* Repeat this block for each product category */ }
+            <div className="col-md-6 col-lg-4 health-container__healthEssentials">
+              <div className="card health-container__card health-container__card--blue">
+                <img className="health-container__icon" src={medicalEquipment} alt="medicalEquipment" />
+              </div>
+              <div className="card-body">
+                <h6>Medical Equipment</h6>
+              </div>
             </div>
-
-            <div class="card-body">
-              <h6>Medical Equipment</h6>
+            <div className="col-md-6 col-lg-4 health-container__healthEssentials">
+              <div className="card health-container__card health-container__card--green">
+                <img className="health-container__icon" src={physiotherapyProduct} alt="physiotherapyProduct" />
+              </div>
+              <div className="card-body">
+                <h6>Physiotherapy Products</h6>
+              </div>
             </div>
-          </div>
-          <div className="healthEssentials col-4">
-            <div className="card cardBorderColor1">
-              <img
-                className="icon"
-                src={physiotherapyProduct}
-                alt="physiotherapyProduct"
-              />
+            <div className="col-md-6 col-lg-4 health-container__healthEssentials">
+              <div className="card health-container__card health-container__card--green">
+                <img className="health-container__icon" src={homeCareDevices} alt="homeCareDevices" />
+              </div>
+              <div className="card-body">
+                <h6>Homecare Devices</h6>
+              </div>
             </div>
-
-            <div class="card-body ">
-              <h6 className="w-2">Physiotherapy Products</h6>
+            <div className="col-md-6 col-lg-4 health-container__healthEssentials">
+              <div className="card health-container__card health-container__card--blue">
+                <img className="health-container__icon" src={generalHealthcareProducts} alt="generalHealthcareProducts" />
+              </div>
+              <div className="card-body">
+                <h6>General Healthcare Products</h6>
+              </div>
             </div>
-          </div>
-          {/* </div>
-          <div className="dataSection"> */}
-          <div className="healthEssentials col-4">
-            <div className="card cardBorderColor1">
-              <img
-                className="icon"
-                src={homeCareDevices}
-                alt="homeCareDevices"
-              />
-            </div>
-
-            <div class="card-body">
-              <h6>Homecare Devices</h6>
-            </div>
-          </div>
-          <div className="healthEssentials col-4">
-            <div className="card cardBorderColor2">
-              <img
-                className="icon"
-                src={generalHealthcareProducts}
-                alt="genaralHealthcareProducts"
-              />
-            </div>
-
-            <div class="card-body">
-              <h6>General Healthcare Products</h6>
-            </div>
-          </div>
+            { /* ... other product categories ... */ }
           </div>
         </div>
       </div>
