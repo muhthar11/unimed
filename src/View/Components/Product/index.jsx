fcf56a25549e04d247aa8b37010fdafb19070ce9
muhthar <muhammedmuhthar11@gmail.com>
2024-04-13 15:01:24 +0530

    fix:product page code issues

--- src/View/Components/Product/index.jsx
+++ src/View/Components/Product/index.jsx
@@ -8,41 +8,53 @@ import product2 from "../../Assets/Images/product2.png";
 import product3 from "../../Assets/Images/product3.png";
 import product4 from "../../Assets/Images/product4.png";
 
-const data = [
-  {
-    image: product1,
-  },
-  {
-    image: product2,
-  },
-  {
-    image: product3,
-  },
-  {
-    image: product4,
-  },
+const products = [
+  { id: 'p1', image: product1, alt: 'Product 1 description' },
+  { id: 'p2', image: product2, alt: 'Product 2 description' },
+  { id: 'p3', image: product3, alt: 'Product 3 description' },
+  { id: 'p4', image: product4, alt: 'Product 4 description' },
 ];
 
 function Product() {
     const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
     };
   return (
     <div className="container-fluid px-0">
       <div className="product-container col-12">
         <Slider {...settings}>
-          {data.map((product, index) => (
-            <div key={index} className="product-slide">
+          {products.map((product) => (
+            <div key={product.id} className="product-slide">
               <img
                 className="product-image"
                 src={product.image}
-                alt={`product${index + 1}`}
+                alt={product.alt}
               />
             </div>
           ))}
